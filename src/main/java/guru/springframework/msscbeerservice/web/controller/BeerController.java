e81a1461fbe3d9878acee68acc1e9bf9d021beca
RK <38069879+angularbyrk@users.noreply.github.com>
2024-02-01 18:12:49 -0600

    Added controller and model files

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer/")
+public class BeerController {
+
+    @GetMapping
+    public ResponseEntity<BeerDto> listAllBeers(){
+        return ResponseEntity.ok(null);
+    }
+    @PostMapping
+    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto){
+        return ResponseEntity.ok(beerDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID id){
+        return ResponseEntity.ok(null);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable UUID id, @RequestBody BeerDto beerDto){
+        return ResponseEntity.ok(beerDto);
+    }
+
+}
---
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -0,0 +1,56 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.swing.*;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BeerController.class)
+class BeerControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void saveNewBeer() throws Exception {
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("corona")
+                .beerStyle(BeerStyleEnum.ALE)
+                .build();
+        String value
+        mockMvc.perform(post("/api/v1/beer/").content(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+
+
+    }
+
+    @Test
+    void getBeerById() throws Exception {
+        mockMvc.perform(post("/api/v1/beer/"+ UUID.randomUUID().toString()).content(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateBeer() throws Exception {
+        mockMvc.perform(post("/api/v1/beer/"+ UUID.randomUUID().toString()).content(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk());
+    }
+}
